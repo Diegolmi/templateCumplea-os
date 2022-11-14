4b2cd5bbca0368e1dc296c4c9fdf5a7c5e9f2202
Diego Olmi <diego.olmi@gmail.com>
2022-11-14 08:18:51 -0300

    acentos

--- src/elements/about/AboutFive.js
+++ src/elements/about/AboutFive.js
@@ -17,8 +17,8 @@ const AboutFive = () => {
                             <div className="col-lg-6 mt_md--30 mt_sm--30">
                                 <div className="content">
                                     <div className="inner">
-                                        <h4 className="title">Está es tu Invitacion</h4>
-                                        <p>Veni a festejar mi cumple!</p>
+                                        <h4 className="title">Esta es tu Invitación</h4>
+                                        <p>Vení a festejar mi cumple!</p>
                                         <ul className="contact-address">
                                             <li><FiDatabase /> Domingo 18 de Diciembre</li>
                                             <li><FiClock /> 14 hs</li>
